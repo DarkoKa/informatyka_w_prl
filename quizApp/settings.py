ddfc38c051351a470d0feaf616d61377aa255f4e
Darko <dariusz.kortas@wp.pl>
2024-12-11 21:37:25 +0100

    Add files via upload

--- quizApp/settings.py
+++ quizApp/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'quiz'
+    'questions'
+    'results'
+
 ]
 
 MIDDLEWARE = [
